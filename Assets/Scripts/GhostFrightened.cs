39b16cc131a91c3aa595f88a0d26e64c01cbb516
Paolo Astolfi <paolo.astolfi@gmail.com>
2022-03-19 11:44:31 +0100

    Implemented Ghost Home Behaviour

--- Assets/Scripts/GhostFrightened.cs
+++ Assets/Scripts/GhostFrightened.cs
@@ -2,5 +2,20 @@ using UnityEngine;
 
 public class GhostFrightened : GhostBehaviour
 {
+    public SpriteRenderer body;
+    public SpriteRenderer eyes;
+    public SpriteRenderer blue;
+    public SpriteRenderer white;
 
+    public bool eaten { get; private set; }
+
+    public override void Enable(float duration)
+    {
+        base.Enable(duration);
+        this.body.enabled = false;
+        this.eyes.enabled = false;
+        this.blue.enabled = true;
+        this.white.enabled = false;
+
+    }
 }
